01a1169e2691a31780833ca4d67d66980fca3ba9
BlishchAndrew <blishch.andrew@gmail.com>
2022-04-08 10:29:42 +0300

    add header-search

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,27 +1,39 @@
 const body = document.querySelector('body'),
 		btnBurger = document.querySelector('.btn-burger-menu'),
 		menuNav = document.querySelector('.header-menu'),
-		menuLinks = document.querySelectorAll('.header-menu__link_btn');
+		menuLinks = document.querySelectorAll('.header-menu__link_btn'),
+		btnShowSearch = document.querySelector('.header-menu__link_btn-search'),
+		headerSearch = document.querySelector('.header-search'),
+		btnCloseSearch = document.querySelector('.header-search__close');
 
 btnBurger.addEventListener('click', function (e) {
-	console.log('ffff');
 	btnBurger.classList.toggle('active');
 	menuNav.classList.toggle('active');
 	body.classList.toggle('lock');
 });
 
 document.addEventListener('click', function (e) {
-   if (!e.target.closest('.btn-burger-menu, .header-menu')) {
+   if (menuNav.classList.contains('active') && !e.target.closest('.btn-burger-menu, .header-menu') ) {
 		btnBurger.classList.remove('active');
 		menuNav.classList.remove('active');
       body.classList.remove('lock');
    }
+	if (headerSearch.classList.contains('active') && !e.target.closest('.header-search, .header-menu__link_btn-search')) {
+		headerSearch.classList.remove('active');
+	}
 });
 menuLinks.forEach(link => {
 	link.addEventListener('click', function (e) {
-		console.log('ffff');
 		link.classList.toggle('active');
 	});
+});
+
+btnShowSearch.addEventListener('click', function (e) {
+	headerSearch.classList.add('active')
+});
+
+btnCloseSearch.addEventListener('click', function (e) {
+	headerSearch.classList.remove('active');
 });
 
 
